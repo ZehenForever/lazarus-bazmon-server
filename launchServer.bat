f5b4eeaa1917150cc95d87b9e709ddfc5eb06adc
ZehenForever <162739753+ZehenForever@users.noreply.github.com>
2024-03-20 12:29:07 -0600

    Create launchServer.bat

---
+++ launchServer.bat
@@ -0,0 +1,8 @@
+@echo off
+title BazMon Server
+echo Starting BazMon Server
+
+rem Replace file path between the quotation marks with the path to your server files
+cd "C:\Path to E3NextAndMQNext\lua\BazMon\server"
+
+rem Replace file path between the quotation marks with the path to your MacroQuest\Config folder
